8a106a7096992f65b75beadb0e4ab1e7ea8fa45a
rakshandajha <rxj7063@rit.edu>
2021-09-13 11:07:32 -0400

    Data encoding using one hot encoding

--- Data Prep/Dataprep.py
+++ Data Prep/Dataprep.py
@@ -1,12 +1,14 @@
 from Bio import SeqIO
 import pandas as pd
+import numpy as np
 import DNA_Seq
+from sklearn.preprocessing import OneHotEncoder
 
 
 def readingFile():
     global avil_sequence, anxa1_sequence, bst2_sequence, cpne4_sequence, cryab_sequence, \
-        gpd2_sequence, hmga2_sequence, krt16_sequence, krt17_sequences, \
-        lcn2_sequence, mtap_sequence
+        gpd2_sequence, hmga2_sequence, krt16_sequence, krt17_sequence, \
+        lcn2_sequence, mtap_sequence, epcam_sequence
     anxa1_sequences = SeqIO.parse(open("../DNA_Seq/ANXA1_mRNA.fasta"), 'fasta')
     avil_sequences = SeqIO.parse(open("../DNA_Seq/AVIL_mRNA.fasta"), 'fasta')
     bst2_sequences = SeqIO.parse(open("../DNA_Seq/BST2_mRNA.fasta"), 'fasta')
@@ -23,58 +25,55 @@ def readingFile():
     for fasta in anxa1_sequences:
         anxa1_name, anxa1_sequence = fasta.id, str(fasta.seq)
         # new_sequence = some_function(sequence)
-        print(anxa1_sequence)
-
     for sequ in avil_sequences:
         avil_name, avil_sequence = sequ.id, str(sequ.seq)
         # new_sequence = some_function(sequence)
-        print(avil_sequence)
     for fasta in bst2_sequences:
         bst2_name, bst2_sequence = fasta.id, str(fasta.seq)
         # new_sequence = some_function(sequence)
-        print(bst2_sequence)
     for fast in cpne4_sequences:
         cpne4_name, cpne4_sequence = fast.id, str(fast.seq)
         # new_sequence = some_function(sequence)
-        print(cpne4_sequence)
     for fasta in cryab_sequences:
         cryab_name, cryab_sequence = fasta.id, str(fasta.seq)
         # new_sequence = some_function(sequence)
-        print(cryab_sequence)
     for fasta in epcam_sequences:
         epcam_name, epcam_sequence = fasta.id, str(fasta.seq)
         # new_sequence = some_function(sequence)
-        print(epcam_sequence)
     for fasta in gpd2_sequences:
         gpd2_name, gpd2_sequence = fasta.id, str(fasta.seq)
         # new_sequence = some_function(sequence)
-        print(gpd2_sequence)
     for fasta in hmga2_sequences:
         hmga2_name, hmga2_sequence = fasta.id, str(fasta.seq)
         # new_sequence = some_function(sequence)
-        print(hmga2_sequence)
     for fasta in krt16_sequences:
         krt16_name, krt16_sequence = fasta.id, str(fasta.seq)
         # new_sequence = some_function(sequence)
-        print(krt16_sequence)
     for fasta in krt17_sequences:
         krt17_name, krt17_sequence = fasta.id, str(fasta.seq)
         # new_sequence = some_function(sequence)
-        print(krt17_sequence)
     for fasta in lcn2_sequences:
         lcn2_name, lcn2_sequence = fasta.id, str(fasta.seq)
         # new_sequence = some_function(sequence)
-        print(lcn2_sequence)
     for fasta in mtap_sequences:
         mtap_name, mtap_sequence = fasta.id, str(fasta.seq)
         # new_sequence = some_function(sequence)
-        print(mtap_sequence)
 
-    return avil_sequence, anxa1_sequence, bst2_sequence, cpne4_sequence, cryab_sequence, \
-           gpd2_sequence, hmga2_sequence, krt16_sequence, krt17_sequences, \
-           lcn2_sequence, mtap_sequence
+    input_sequences = {"ANXA1":anxa1_sequence, "AVIL" : avil_sequence,
+                       "BST2":bst2_sequence, "CPNE4":cpne4_sequence, "CRYAB":cpne4_sequence,
+                       "EPCAM":epcam_sequence, "GPD2":gpd2_sequence, "HMGA2": hmga2_sequence,
+                       "KRT16":krt16_sequence, "KRT17":krt17_sequence, "LCN2":lcn2_sequence, "MTAP":mtap_sequence
+                       }
+    return input_sequences
 
-
+def dataEncoding(input, orf):
+    input = list(input.items())
+    data = np.array(input).reshape(-1,1)
+    encoder = OneHotEncoder(handle_unknown='ignore')
+    encoder.fit(data)
+    data_list = encoder.categories
+    encoder.transform(data).toarray()
+    print(1)
 def main():
     start = "ATG"
     end = "TAG, TAA, TGA"
@@ -96,8 +95,8 @@ def main():
         "LCN2": "ATGGTGTTCGGGCTGGTGCGGCAGCTGGCGGCACCTGTGCACTCAGCCGTCGATACACTGGTCGATTGGGACAGGGAAGACGATGTGGTTTTCAGGGAGGCCCAGAGATTTGGAGAAGCGGATGAAGTTCTCCTTTAG",
         "MTAP" : "ATGGATGAGCAACTGTGCCCGGCCAACAAAAAGCATTTCTATAAAATAACAATAAAAAAAGAGTTAAAAAAGCAAGCTTCCACTTTATTAAAAAGCAAAAATTACCAACTCGTATAA"
     }
-    print(start + " " + end)
-    readingFile()
-
+    input_sequences = {}
+    input_sequences = readingFile()
+    dataEncoding(input=input_sequences, orf=Orf)
 
 main()
